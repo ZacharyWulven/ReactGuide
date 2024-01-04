cbc873f8348c6bb493623cd6e6b383972541e09e
Zachary <poisontusk@gmail.com>
2024-01-04 17:50:28 +0800

    对传入子组件 props 进行限制

--- app/todo_list_demo_m1/src/components/Header/index.jsx
+++ app/todo_list_demo_m1/src/components/Header/index.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
 import "./index.css";
+// 安装依赖，$ yarn add prop-types
+import PropTypes from "prop-types";
 
-// 安装 nanoid，$ yarn add nanoid@4.0.0
+// 安装依赖 nanoid，$ yarn add nanoid@4.0.0
 import { nanoid } from "nanoid";
 
 export default class Header extends Component {
+  // 对接收 props 进行：类型 必要性的限制
+  // Note: 这版本不用加 static
+  PropTypes = {
+    callbackOfAdd: PropTypes.func.isRequired,
+  };
+
   // 事件对象就是点击的标签对象，直接用参数就行，无需使用 ref
+  // 键盘事件回调
   handleKeyUp = (event) => {
     // 13 是回车按钮的号码
     // 按下回车才往下走
--- app/todo_list_demo_m1/src/components/List/index.jsx
+++ app/todo_list_demo_m1/src/components/List/index.jsx
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
 import Item from "../Item";
 import "./index.css";
+// 安装依赖，$ yarn add prop-types
+import PropTypes from "prop-types";
 
 export default class List extends Component {
+  // 对接收 props 进行：类型 必要性的限制
+  PropTypes = {
+    todolist: PropTypes.array.isRequired,
+    callbackOfChecked: PropTypes.func.isRequired,
+  };
   render() {
     const { todolist, callbackOfChecked } = this.props;
     console.log("List ", todolist);
