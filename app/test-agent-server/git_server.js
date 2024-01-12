07e3c0a06fb62aaa3029e4524e29e97f25e340cf
Zachary <poisontusk@gmail.com>
2024-01-12 17:37:46 +0800

    history

--- app/test-agent-server/git_server.js
+++ app/test-agent-server/git_server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const axios = require("axios");
+
+// 请求地址 http://localhost:3000/search/users?q=??
 
 app.use((request, response, next) => {
   console.log("有人请求了 server 1");
@@ -9,19 +12,22 @@ app.use((request, response, next) => {
   next();
 });
 
-app.get("/search/users", (request, response) => {
-  const students = [
-    { id: "001", name: "tom", age: 18 },
-    { id: "002", name: "jerry", age: 28 },
-    { id: "003", name: "tony", age: 35 },
-  ];
-  response.send(students);
+// 请求路径为 /search/users
+app.get("/search/users", (request, res) => {
+  const { q } = request.query;
+  axios({
+    url: "https://api.github.com/search/users",
+    params: { q },
+  }).then((response) => {
+    res.json(response.data);
+  });
+  // response.send(students);
 });
 
 app.listen(5000, (err) => {
   if (!err) {
     console.log(
-      "server 1 启动成功了，请求 git 用户信息地址为 http://localhost:5000/search/users?q=??"
+      "server 启动成功了，请求 git 用户信息地址为 http://localhost:5000/search/users?q=??"
     );
   }
 });
