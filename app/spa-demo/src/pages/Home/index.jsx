5b2b9a26cacdf02aa1a434305f24167aec3ff726
Zachary <poisontusk@gmail.com>
2024-01-17 17:37:12 +0800

    嵌套路由

--- app/spa-demo/src/pages/Home/index.jsx
+++ app/spa-demo/src/pages/Home/index.jsx
@@ -1,7 +1,35 @@
 import React, { Component } from "react";
+import Messages from "./Messages";
+import { NavLink, Route, Switch, Redirect } from "react-router-dom";
+import News from "./News";
+import MyNavLink from "../../components/MyNavLink";
 
 export default class Home extends Component {
   render() {
-    return <div>我是 Home 的内容</div>;
+    return (
+      <div>
+        <h3>我是 Home 组件内容</h3>
+        <div>
+          <ul className="nav nav-tabs">
+            <li>
+              <MyNavLink to="/home/news">News</MyNavLink>
+            </li>
+            <li>
+              <MyNavLink to="/home/messages">Messages</MyNavLink>
+            </li>
+          </ul>
+          <div>
+            {/* 注册路由 */}
+            <Switch>
+              {/* 人家要的是 /home */}
+              <Route path="/home/news" component={News} />
+              <Route path="/home/messages" component={Messages} />
+              {/* Redirect 可以用来兜底 */}
+              <Redirect to="/home/news" />
+            </Switch>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
