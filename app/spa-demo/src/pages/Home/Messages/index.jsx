f48c6d24a011558b17873ef3e77b9a9afcf46d4e
Zachary <poisontusk@gmail.com>
2024-01-23 18:57:01 +0800

    编程式路由导航

--- app/spa-demo/src/pages/Home/Messages/index.jsx
+++ app/spa-demo/src/pages/Home/Messages/index.jsx
@@ -11,6 +11,52 @@ export default class Messages extends Component {
     ],
   };
 
+  // 编程式路由导航：push
+  pushShow = (id, title) => {
+    // 实现跳转到 Detail 组件，并且为 push 跳转
+
+    // 1 编程式路由导航：push 跳转 + 传递 params 参数
+    // this.props.history.push(`/home/messages/detail/${id}/${title}`);
+
+    // 2 编程式路由导航：push 跳转 + 传递 query 参数
+    // this.props.history.push(`/home/messages/detail?id=${id}&title=${title}`);
+
+    // 3 编程式路由导航：push 跳转 + 传递 state 参数，传入的第二个参数就是 state
+    this.props.history.push(`/home/messages/detail`, {
+      id,
+      title,
+    });
+  };
+
+  // 编程式路由导航：replace
+  replaceShow = (id, title) => {
+    // 实现跳转到 Detail 组件，并且为 replace 跳转
+    console.log("replaceShow: ", id, title);
+
+    // 1 编程式路由导航：replace 跳转 + 传递 params 参数
+    // this.props.history.replace(`/home/messages/detail/${id}/${title}`);
+
+    // 2 编程式路由导航：replace 跳转 + 传递 query 参数
+    // this.props.history.replace(`/home/messages/detail?id=${id}&title=${title}`);
+
+    // 3 编程式路由导航：replace 跳转 + 传递 state 参数，传入的第二个参数就是 state
+    this.props.history.replace(`/home/messages/detail`, {
+      id,
+      title,
+    });
+  };
+
+  goBack = () => {
+    this.props.history.goBack();
+  };
+
+  goForward = () => {
+    this.props.history.goForward();
+  };
+  go = () => {
+    // 写死前进 2 步
+    this.props.history.go(2);
+  };
   render() {
     const { messages } = this.state;
     return (
@@ -23,14 +69,12 @@ export default class Messages extends Component {
                 {/* <Link to={`/home/messages/detail/${msg.id}/${msg.title}`}>
                   {msg.title}
                 </Link> */}
-
                 {/* 2 向路由组件传递 search 参数 */}
                 {/* <Link
                   to={`/home/messages/detail/?id=${msg.id}&title=${msg.title}`}
                 >
                   {msg.title}
                 </Link> */}
-
                 {/* 3 向路由组件传递 state 参数 */}
                 <Link
                   replace
@@ -41,6 +85,23 @@ export default class Messages extends Component {
                 >
                   {msg.title}
                 </Link>
+                {/* button */}
+                &nbsp;&nbsp;
+                <button
+                  onClick={() => {
+                    this.pushShow(msg.id, msg.title);
+                  }}
+                >
+                  push 查看
+                </button>
+                &nbsp;&nbsp;
+                <button
+                  onClick={() => {
+                    this.replaceShow(msg.id, msg.title);
+                  }}
+                >
+                  replace 查看
+                </button>
               </li>
             );
           })}
@@ -48,15 +109,16 @@ export default class Messages extends Component {
         <hr />
         <div>
           {/* 注册路由 */}
-
           {/* 1 声明接收 params 参数 */}
           {/* <Route path="/home/messages/detail/:id/:title" component={Detail} /> */}
-
           {/* 2 search 参数无需声明接收，正常注册路由即可 */}
           {/* <Route path="/home/messages/detail" component={Detail} /> */}
-
           {/* 3 state 参数无需声明接收，正常注册路由即可 */}
           <Route path="/home/messages/detail" component={Detail} />
+          {/* add back and forward button */}
+          &nbsp;&nbsp;<button onClick={this.goBack}>回退</button>
+          &nbsp;&nbsp;<button onClick={this.goForward}>前进</button>
+          &nbsp;&nbsp;<button onClick={this.go}>Go</button>
         </div>
       </div>
     );
